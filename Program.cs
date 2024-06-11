9c46384460e0da552e9afd4f5f0a214f3cd8c805
Marcio Rocha <dev.marcio.rocha@gmail.com>
2024-06-11 11:04:10 -0300

    corrigido código para melhor performance

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Text;
 
 namespace ProjetoLoteria;
 
@@ -7,14 +8,39 @@ public class Program
     public static void Main()
     {
         const int tamanhoConjuntoUniverso = 25;
-        const int limiteInferior = 11;
+        #region Get user input for combinacao menor
+        Console.Write("Informar a combinação menor (Integer input): ");
+        string userInput = Console.ReadLine()!;
+        int number;
+        bool success = Int32.TryParse(userInput, out number);
+        if (!success)
+        {
+            Console.WriteLine("You entered an invalid integer number: " + userInput);
+            Environment.Exit(0);
+        } 
+        int limiteInferior = number;
+        #endregion
+        #region Get user input for Heuristica
+        Console.Write("Informar o ajuste da heuristica (Integer input): ");
+        userInput = Console.ReadLine()!;
+        success = Int32.TryParse(userInput, out number);
+        if (!success)
+        {
+            Console.WriteLine("You entered an invalid integer number: " + userInput);
+            Environment.Exit(0);
+        }
+
+        int percHeuristica = number;
+        #endregion
+        
         const int limiteSuperior = 15;
         
         #region Gerando Conjuntos de combinações
+        Console.WriteLine($"Gerando os conjuntos das combinações [C25,15 C25,{limiteInferior}]");
         var conjuntoUniverso = new List<int>();
         for (int i = 0; i < tamanhoConjuntoUniverso; i++) conjuntoUniverso.Add(i);
         var results = new ConcurrentDictionary<int, ConcurrentBag<List<int>>>();
-        Parallel.For(limiteInferior, limiteSuperior+1, i =>
+        Parallel.ForEach([limiteInferior, limiteSuperior], i =>
         {
             var combinations = new ConcurrentBag<List<int>>();
             foreach (var combination in GetCombinations(conjuntoUniverso, i))
@@ -24,34 +50,9 @@ public class Program
             results[i] = combinations;
         });
         #endregion
-        // E.g. to get combinations for C25,15 you can use results[15]
-        #region contém todas as combinações em formato de IList (otimização)
-
-        List<List<int>> c2514Sequences = new List<List<int>>(); 
-        List<List<int>> c2513Sequences = new List<List<int>>();
-        List<List<int>> c2512Sequences = new List<List<int>>();
-        List<List<int>> c2511Sequences = new List<List<int>>();
-        if (results.ContainsKey(14))
-        {
-            results.Remove(14, out var auxOut);
-            c2514Sequences = auxOut!.Select(list => list.ToList()).ToList();
-        }
-        if (results.ContainsKey(13))
-        {
-            results.Remove(13, out var auxOut);
-            c2513Sequences = auxOut!.Select(list => list.ToList()).ToList();
-        }
-        if (results.ContainsKey(12))
-        {
-            results.Remove(12, out var auxOut);
-            c2512Sequences = auxOut!.Select(list => list.ToList()).ToList();
-        }
-        if (results.ContainsKey(11))
-        {
-            results.Remove(11, out var auxOut);
-            c2511Sequences = auxOut!.Select(list => list.ToList()).ToList();
-        }
-        #endregion 
+        
+        // to get combinations for C25,15 you can use results[15]
+      
         
         #region CheckSubSequence
         // Função que verifica se uma lista é um subconjunto de outra lista.
@@ -61,74 +62,55 @@ public class Program
             return subset.All(hashSet.Contains);
         };
         #endregion
-        
-        #region containers para armazenamento de sequencias com subsequencias
-        // Prepare um contêiner para armazenar as sequências C25,15 que contêm todas as sequências C25,14.
-        List<int> c25_15_SubsetsWithAllC25_14 = new List<int>();
-        List<int> c25_15_SubsetsWithAllC25_13 = new List<int>();
-        List<int> c25_15_SubsetsWithAllC25_12 = new List<int>();
-        List<int> c25_15_SubsetsWithAllC25_11 = new List<int>();
-        #endregion
 
         #region For loop variables
-        int qtdC25_14 = 0, qtdC25_13 = 0, qtdC25_12 = 0, qtdC25_11 = 0, max14 = 0, max13 = 0, max12 = 0, max11 = 0;
+        int qtdC25_n = 0, maxN = 0;
+        List<int> c25_15_SubsetsWithAllC25_N = new List<int>();
         #endregion
         
         #region Para cada sequência C25,15 ...
-        foreach (var c25_15_Sequence in results[15].Select(list => list.ToList()))
+        foreach (var c25_15_Sequence in results[15].ToArray())
         {
+            // Heuristica com base na % do grupo preenchido
+            if (((double)percHeuristica / 10000) <= ((double)maxN / results[limiteInferior].Count))
+                break;
             // Verifique se ela contém alguma sequência C25,14.
             #region count sub-sequences
-            qtdC25_14 = c2514Sequences.Where(seq => CheckSubsequence(seq, c25_15_Sequence)).Count();
-            qtdC25_13 = c2513Sequences.Where(seq => CheckSubsequence(seq, c25_15_Sequence)).Count();
-            qtdC25_12 = c2512Sequences.Where(seq => CheckSubsequence(seq, c25_15_Sequence)).Count();
-            qtdC25_11 = c2511Sequences.Where(seq => CheckSubsequence(seq, c25_15_Sequence)).Count();
-            #endregion
+            Console.WriteLine($"Verificando combinações agregadas para [{ImprimirListaConjunto(c25_15_Sequence)}]...");
+            // heurística para pular a diferença entre os conjuntos, para aumentar a variedade entre as comparações
+            // funciona como uma espécie de shuffle
+            if (0 != results[15].ToList().IndexOf(c25_15_Sequence) % limiteInferior)
+            {
+                continue;
+            }
 
+            qtdC25_n = results[limiteInferior].Where(seq => CheckSubsequence(seq, c25_15_Sequence)).Count();
+            #region check C25,limiteInferior
+            if (maxN >= qtdC25_n)
+            {
+                continue;
+            }
+            Console.WriteLine($"Nova combinação com maior agregação para C25,{limiteInferior}");
+            c25_15_SubsetsWithAllC25_N = c25_15_Sequence;
+            maxN = qtdC25_n;
+            #endregion
+            
+            #endregion
             // Se c25_15_Sequence contém todas as sequências C25,14, adicione-a ao nosso contêiner.
-            #region check C25,14
-            if (max14 <= qtdC25_14)
-            {
-                c25_15_SubsetsWithAllC25_14 = c25_15_Sequence;
-                max14 = qtdC25_14;
-            }
-            #endregion
-            
-            #region check C25,13
-            if (max13 <= qtdC25_13)
-            {
-                c25_15_SubsetsWithAllC25_13 = c25_15_Sequence;
-                max13 = qtdC25_13;
-            }
-            #endregion
-            
-            #region check C25,12
-            if (max12 <= qtdC25_12)
-            {
-                c25_15_SubsetsWithAllC25_12 = c25_15_Sequence;
-                max12 = qtdC25_12;
-            }
-            #endregion
-
-            #region check C25,11
-            if (max11 <= qtdC25_11)
-            {
-                c25_15_SubsetsWithAllC25_11 = c25_15_Sequence;
-                max11 = qtdC25_11;
-            }
-            #endregion
-            
         }
         #endregion
-        
-        
-        Console.WriteLine("============================================================");
-        Console.WriteLine("Resultados:");
-        Console.WriteLine($"<<<<<<\nC25,15 que melhor agrega C25,14:\n[{ImprimirListaConjunto(c25_15_SubsetsWithAllC25_14)}]\nSub conjuntos identificados para esta sequencia: {max14}");
-        Console.WriteLine($"<<<<<<\nC25,15 que melhor agrega C25,13:\n[{ImprimirListaConjunto(c25_15_SubsetsWithAllC25_13)}]\nSub conjuntos identificados para esta sequencia: {max13}");
-        Console.WriteLine($"<<<<<<\nC25,15 que melhor agrega C25,12:\n[{ImprimirListaConjunto(c25_15_SubsetsWithAllC25_12)}]\nSub conjuntos identificados para esta sequencia: {max12}");
-        Console.WriteLine($"<<<<<<\nC25,15 que melhor agrega C25,11:\n[{ImprimirListaConjunto(c25_15_SubsetsWithAllC25_11)}]\nSub conjuntos identificados para esta sequencia: {max11}");
-        Console.WriteLine();
+
+
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine("============================================================")
+            .AppendLine("Resultados:")
+            .AppendLine($"<<<<<<\nC25,15 que melhor agrega C25,{limiteInferior}:\n[{ImprimirListaConjunto(c25_15_SubsetsWithAllC25_N)}]\nSub conjuntos identificados para esta sequencia: {maxN}");
+        Console.WriteLine(sb.ToString());
+        string fileOutput = Path.Combine(Directory.GetCurrentDirectory(), $"ExportResult_C25-{limiteSuperior}_C25-{limiteInferior}.txt");
+        using (StreamWriter streamWriter = new StreamWriter(fileOutput))
+        {
+            streamWriter.Write(sb.ToString());
+        }
     }
     
     public static IEnumerable<IEnumerable<T>> GetCombinations<T>(List<T> list, int length)
